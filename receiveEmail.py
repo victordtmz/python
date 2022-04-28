9ebd588349c9cd513290f5b432dd56c5d3c6cad5
Victor Martinez <victordmtz@hotmail.com>
2022-04-27 17:09:36 -0700

    send and receive email

---
+++ receiveEmail.py
@@ -0,0 +1,22 @@
+import imaplib
+import email
+imap_server = "imap.flockmail.com"
+account = 'victor.m@enlacellc.com'
+pwd = "Abogado2020"
+imap = imaplib.IMAP4_SSL(imap_server)
+imap.login(account, pwd)
+imap.select("Inbox")
+_, msgnums = imap.search(None, "ALL")
+print(msgnums)
+for msgnum in msgnums[0].split():
+    _, data = imap.fetch(msgnum, "(RFC822)")
+    message = email.message_from_bytes(data[0][1])
+    # print(message.get("From"))
+    # print(message.get("To"))
+    # print(message.get("BCC"))
+    # print(message.get("Date"))
+    # print(message.get("Subject"))
+    print("Content:")
+    for part in message.walk():
+        if part.get_content_type() == "text/html":
+            print(part.as_string())
---
+++ sentemail.py
@@ -0,0 +1,38 @@
+from fileinput import filename
+import smtplib 
+from email.message import EmailMessage
+import imghdr
+
+msg = EmailMessage()
+msg["Subject"] = "This is to test my Email"
+msg["From"] = 'victor.m@enlacellc.com'
+msg["To"] = 'victordmtz@hotmail.com'
+#this option if for plain text
+content = f""" This <h1>email</h1> it to test the functionality
+    of my messages or may 
+    email service    
+    """
+msg.set_content(content)
+# for html mail
+msg.add_alternative(F'''\
+    <!DOCTYPE html>
+    <h1>This is an HTML Message</h1>
+    <p>{content}</p>
+    </html>
+''', subtype="html")
+
+with open("account.png", "rb") as f:
+    file_data = f.read()
+    file_type = imghdr.what(f.name)
+    file_name = f.name
+# Name is optional, but if not added, on will be given automatically. 
+# use loop for multiple files
+#pdf - maintype = Application .... subtype= octet-stream (generic information)
+msg.add_attachment(file_data, maintype = "image", subtype = file_type, filename=file_name )
+
+with smtplib.SMTP_SSL('smtp.flockmail.com',465) as smtp:
+    account = 'victor.m@enlacellc.com'
+    pwd = "Abogado2020"
+    smtp.login(account, pwd)
+    smtp.send_message(msg)
+    
